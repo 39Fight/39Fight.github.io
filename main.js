4d52f2d5117fe8ad89bb4c7f68a3c66c5ccdf874
39Fight-Admin <39fight.team@gmail.com>
2025-06-24 23:19:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -42,3 +42,11 @@ function onSignIn(googleUser) {
   $(".data").css("display","block");
   $(".g-signin2").css("display","none");
 }
+
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    });
+  }
+</script>
