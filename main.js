26e2318c2150199f5495b90952ce7d17d25d80d0
39Fight-Admin <39fight.team@gmail.com>
2025-06-24 23:13:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -31,3 +31,13 @@ function oauthSignIn() {
   document.body.appendChild(form);
   form.submit();
 }
+
+// Get profile information
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
